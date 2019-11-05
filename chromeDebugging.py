77e4fd45a5999d1350537d33fc1afe8760d0d32c
intel <intelppt14@gmail.com>
2019-11-05 11:23:56 -0800

    added .gitignore file

--- chromeDebugging.py
+++ chromeDebugging.py
@@ -10,16 +10,24 @@ import logging
 import logging.handlers
 
 
-LOG_FILENAME = datetime.now().strftime('coldboot_logfile_%H_%M_%d_%m_%Y.log')
-LOG_FILENAME_STR = os.path.abspath(LOG_FILENAME)
-# print ("Log file name is: %s"% LOG_FILENAME)
-logging.basicConfig(filename=LOG_FILENAME, stream=sys.stdout, level=logging.DEBUG, )
+chrome_debug_log_folder = os.getcwd() + "/debug_log"
+if not os.path.exists(chrome_debug_log_folder):
+    os.makedirs(chrome_debug_log_folder)
+
+log_file_name = chrome_debug_log_folder + "/" + datetime.now().strftime('%Y-%m-%d_%H:%M:%S') +"_chrome_debug.log"
+#lname = datetime.now().strftime('%Y-%m-%d_%H:%M') +"_chrome_debug.log"
+
+logging.basicConfig(filename= log_file_name, level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler = logging.StreamHandler(sys.stdout)
+dlogger = logging.getLogger(__name__)
+dlogger.addHandler(handler)
+
 
 #CONFIG PARAMETERS FOR USER TO CHANGE
 cros_sdk_path = '/home/cssdesk/google_source'
 abs_cros_sdk_path = '/home/cssdesk/depot_tools/cros_sdk --no-ns-pid'
 shutdown_wait_time = 16
-reboot_wait_time = 1500
+reboot_wait_time = 80
 #END CONFIG PARAMETERS FOR USER TO CHANGE
 
 def check_if_remote_system_is_live(ip):
@@ -171,16 +179,13 @@ def servod_process(cros_sdk_path, abs_cros_sdk_path):
 
     if out:
         servod_pid = int(out.strip())
-        print('servod Process found.')
-        logging.info('servod Process found.')	
+        dlogger.info('servod Process found.')
         return True
 
-    print('starting a fresh servod...')
-    logging.info('starting a fresh servod...')
+    dlogger.info('starting a fresh servod...')
 
     os.chdir(cros_sdk_path)	
-    print (os.getcwd())
-    logging.info (os.getcwd())
+    dlogger.info (os.getcwd())
     
 
     servod_cmd = 'python ' + abs_cros_sdk_path + ' ' + 'sudo ' + 'servod ' + '--board=cyan ' + '&'
@@ -191,31 +196,26 @@ def servod_process(cros_sdk_path, abs_cros_sdk_path):
     output = subprocess.Popen(['pgrep', 'servod'], stdout=subprocess.PIPE).communicate()[0]
 
     if output:
-        print "Servod started successfully"
-        logging.info("Servod started successfully")
+        dlogger.info("Servod started successfully")
         return True
     else:
-        print "Servod couldn't be started successfully. Exiting test."
-        logging.info("Servod couldn't be started successfully. Exiting test.")
+        dlogger.info("Servod couldn't be started successfully. Exiting test.")
         return False
     
 
 def servo_coldboot(dut_ip, username="root", password="test0000"):
     pwr_btn_command = 'python' + ' ' + abs_cros_sdk_path + ' ' + 'dut-control pwr_button:press sleep:0.5 pwr_button:release'
-    print ("Sending shutdown command to the DUT")
-    logging.info ("Sending shutdown command to the DUT")
+    ec_reset_command = 'python' + ' ' + abs_cros_sdk_path + ' ' + 'dut-control ec_uart_cmd:reboot'
+    dlogger.info ("Sending shutdown command to the DUT")
     sshpassCmd = "sshpass -p " + password + " ssh -o StrictHostKeyChecking=no " + username + "@" + dut_ip + " 'shutdown -P now'"
-    print (sshpassCmd)
-    logging.info (sshpassCmd)
+    dlogger.info (sshpassCmd)
     p = subprocess.Popen(sshpassCmd, stdout=subprocess.PIPE, shell=True)
     time.sleep(shutdown_wait_time)
     if not check_if_remote_system_is_live(dut_ip):
-        print ("System shutdown successfull.")
-        logging.info ("System shutdown successfull.")
+        dlogger.info ("System shutdown successfull.")
 	
     os.chdir(cros_sdk_path)	
-    print (os.getcwd())
-    logging.info (os.getcwd())
+    dlogger.info (os.getcwd())
     
     ec_uart_capture_enable_command = 'python' + ' ' + abs_cros_sdk_path + ' ' + 'dut-control ec_uart_capture:on'
     ec_uart_capture_disable_command = 'python' + ' ' + abs_cros_sdk_path + ' ' + 'dut-control ec_uart_capture:off'
@@ -225,46 +225,45 @@ def servo_coldboot(dut_ip, username="root", password="test0000"):
     os.system(ec_console_system_status_command)
     system_status_check = os.popen(ec_console_system_status_output).read()
     os.system(ec_uart_capture_disable_command)
-    print (system_status_check)
-    logging.info (system_status_check)
+    dlogger.info (system_status_check)
     
     if system_status_check.find("G3") != -1:
-        print ("System successfully went to G3")
-        logging.info ("System successfully went to G3")
+        dlogger.info ("System successfully went to G3")
     else:
-        print ("System is not going to G3. Exiting test")
-        logging.info ("System is not going to G3. Exiting test")
+        dlogger.info ("System is not going to G3. Exiting test")
         return False
 		
-    print ("Pressing powerbtn to poweron system")
-    logging.info ("Pressing powerbtn to poweron system")
+    dlogger.info ("Pressing powerbtn to poweron system")
     for i in range(3):
         os.system(pwr_btn_command)
-        print ("Waiting 25 minutes for the system to come up.")
-        logging.info ("Waiting 25 minutes for the system to come up.")
+        dlogger.info ("Waiting for %d minutes for the system to come up." %(reboot_wait_time/60))
         for i in range(reboot_wait_time):
-            time.sleep(1)
+            time.sleep(3)
             if check_if_remote_system_is_live(dut_ip):
-                print ("Waiting for device initialization after test in seconds: 10")
-                logging.info ("Waiting for device initialization after test in seconds: 10")
+                dlogger.info("Waiting 10 seconds for device initialization after system boot.")
                 time.sleep(10)
                 return True
-    else:
-        print ("Powerbtn wake didnt work even after 3 attempts. Exiting Test")
-        logging.info ("Powerbtn wake didnt work even after 3 attempts. Exiting Test")
-        return False
+
+    dlogger.info ("Powerbtn wake didnt work even after 3 attempts. Will try Ec reset to recover system")
+    os.system(ec_reset_command)
+    dlogger.info("Waiting for %d minutes after ec reset for the system to come up." % (reboot_wait_time / 60))
+    for i in range(reboot_wait_time):
+        time.sleep(3)
+        if check_if_remote_system_is_live(dut_ip):
+            dlogger.info("Waiting 10 seconds for device initialization after system boot.")
+            time.sleep(10)
+            dlogger.info("Able to recover system with ec reset.")
+            return True
+    dlogger.info("3 attempts of powerbtn wake and 1 attempt of ec reset failed to recover system. Exiting test.")
+    return False
   
-
-
 
 
 if __name__ == "__main__":
 
     if not is_tool("sshpass"):
-        print ("sshpass is not installed. Please install sshpass with sudo apt-get install sshpass")
-        logging.info ("Exiting test!")
-        logging.info ("sshpass is not installed. Please install sshpass with sudo apt-get install sshpass")
-        logging.info ("Exiting test!")
+        dlogger.info ("sshpass is not installed. Please install sshpass with sudo apt-get install sshpass")
+        dlogger.info ("Exiting test!")
         sys.exit(1)
 
     parser = argparse.ArgumentParser()
@@ -286,8 +285,7 @@ if __name__ == "__main__":
         ip_address = args.ip_address
     else:
         ip_address = False
-        print ("check with --help or give cmd argument --IP <ip_address>")
-        logging.info ("check with --help or give cmd argument --IP <ip_address>")
+        dlogger.info ("check with --help or give cmd argument --ip <ip_address>")
         sys.exit(1)
         
     
@@ -298,14 +296,10 @@ if __name__ == "__main__":
     
     if test_to_run == "servo_coldboot":
         if servod_process(cros_sdk_path, abs_cros_sdk_path):
-            print ("Servod PASS. Will continue test.")
-            logging.info ("Servod PASS. Will continue test.")
-           
+            dlogger.info ("Servod PASS. Will continue test.**************")
         else:
-            print ("Servod not running.")
-            print ("Unable to start servod. Exiting test.")
-            logging.info ("Servod not running.")
-            logging.info ("Unable to start servod. Exiting test.")
+            dlogger.info ("Servod not running.")
+            dlogger.info ("Unable to start servod. Exiting test.")
             sys.exit()
 
     print ("Testcase selected to run                                 :", testcase)
@@ -331,21 +325,15 @@ if __name__ == "__main__":
     count = 1
     
     while (count < int(iteration_count)):
-        print ("******************************")
-        print ("******************************")
-        logging.info ("******************************")
-        logging.info ("******************************")
-        print ("STARTING ITERATION %d of %d" % (count, int(iteration_count)))
-        logging.info ("STARTING ITERATION %d of %d" % (count, int(iteration_count)))
-        print ("******************************")
-        print ("******************************")
-        logging.info ("******************************")
-        logging.info ("******************************")
+        dlogger.info ("******************************")
+        dlogger.info ("******************************")
+        dlogger.info ("STARTING ITERATION %d of %d" % (count, int(iteration_count)))
+        dlogger.info ("******************************")
+        dlogger.info ("******************************")
 
         if testcase:
             testcase_output = run_command(testcase, ip_address, username="root", password="test0000")
-            print (testcase_output)
-            logging.info (testcase_output)
+            dlogger.info (testcase_output)
             time.sleep(5)
 
         if test_to_run == "suspend":
@@ -361,27 +349,19 @@ if __name__ == "__main__":
         
         count = count + 1
         cmd_output = run_command(cmd_to_run, ip_address, username="root", password="test0000")
-        print (cmd_output)
-        logging.info (cmd_output)
+        dlogger.info (cmd_output)
         if cmd_output:       
             if searchPatternMatched(cmd_output, pattern_list):
                 break
             else:
-                print ("cmd %s successfull!"%(cmd_to_run))
-                logging.info ("cmd %s successfull!"%(cmd_to_run))
+                dlogger.info ("cmd %s successfull!"%(cmd_to_run))
         else:
-            print ("please check the command you are trying!")
-            logging.info ("please check the command you are trying!")
+            dlogger.info ("please check the command you are trying!")
             break
-    print ("******************************")
-    print ("******************************")
-    logging.info ("******************************")
-    logging.info ("******************************")
-    print ("Completed ITERATION %d of %d" % (count, int(iteration_count))) 
-    logging.info ("Completed ITERATION %d of %d" % (count, int(iteration_count)))        
-    print ("******************************")
-    print ("******************************")
-    logging.info ("******************************")
-    logging.info ("******************************")
+    dlogger.info ("******************************")
+    dlogger.info ("******************************")
+    dlogger.info ("Completed ITERATION %d of %d" % (count, int(iteration_count)))
+    dlogger.info ("******************************")
+    dlogger.info ("******************************")
 
             
